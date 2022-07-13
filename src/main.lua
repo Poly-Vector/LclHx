71ba6b2764e44b2176f1c8a26594593c53fa67e1
PolyVector <86744034+Poly-Vector@users.noreply.github.com>
2022-07-13 20:29:15 +0100

    Edited UI

--- src/main.lua
+++ src/main.lua
@@ -1,8 +1,10 @@
 local LclHx = {}
 LclHx.__index = LclHx
 
+local LclHx_UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/LclHx/LclHx-UI/master/src/ui-lib.lua"))()
+
 local function CreateUi()
-	
+	local UI = LclHx_UI.CreateTitleWindow("LclHx", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"), UDim2.new(0, 300, 0, 50), UDim2.new(0, 0, 0, 0), UDim2.new(0, 0, 0, 0))
 end
 
 return LclHx
--- src/ui-lib.lua
+++ src/ui-lib.lua
@@ -6,20 +6,24 @@ local PlayerGui = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui
 
 
 function LclHx_UI.CreateTitleWindow(Title: string, Parent: Instance,  Size: UDim2, Position, UDim2)
-    local Window = Instance.new("ScreenGui")
-    Window.Parent = Parent
-    Window.Name = Title
-
     local Frame = Instance.new("Frame")
-    Frame.Parent = Window
+    Frame.Parent = Parent
     Frame.Name = "WindowFrame"
 
     Frame.Size = Size
     Frame.Position = Position
+    Frame.AnchorPoint = UDim2(0.5, 0, 0.5, 0)
     Frame.BorderSizePixel = 1.5
 
     Frame.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
     Frame.BorderColor3 = Color3.fromRGB(48, 48, 48)
+
+    local TitleLabel = Instance.new("TextLabel")
+    TitleLabel.Parent = Frame
+    TitleLabel.Name = Title
+
+    TitleLabel.Size = UDim2.new(1, 0, 0.1, 0)
+    TitleLabel.Position = UDim2.new(0.5, 0, 0.1, 0)
 end
 
 return LclHx_UI
