a2cde363c66032930f297a2b283c66ac0749bb9c
PolyVector <86744034+Poly-Vector@users.noreply.github.com>
2022-07-13 20:32:07 +0100

    Fixed UI

--- src/main.lua
+++ src/main.lua
@@ -1,10 +1,14 @@
+local StarterGui = game:GetService("StarterGui")
 local LclHx = {}
 LclHx.__index = LclHx
 
 local LclHx_UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/LclHx/LclHx-UI/master/src/ui-lib.lua"))()
 
 local function CreateUi()
-	local UI = LclHx_UI.CreateTitleWindow("LclHx", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"), UDim2.new(0, 300, 0, 50), UDim2.new(0, 0, 0, 0), UDim2.new(0, 0, 0, 0))
+	local PlayerGui = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+
+	local Window = Instance.new(StarterGui, PlayerGui)
+	local UI = LclHx_UI.CreateTitleWindow("LclHx", Window, UDim2.new(0, 300, 0, 50), UDim2.new(0, 0, 0, 0), UDim2.new(0, 0, 0, 0))
 end
 
 return LclHx
